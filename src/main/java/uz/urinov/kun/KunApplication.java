97c10fb954dadad8651974c164af7ff6d1673481
urinovshokirjon <urinovshokirjon93@gmail.com>
2024-05-17 16:29:54 +0500

    firstCommit

---
+++ src/main/java/uz/urinov/kun/KunApplication.java
@@ -0,0 +1,13 @@
+package uz.urinov.kun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KunApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/urinov/kun/KunApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.urinov.kun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KunApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
