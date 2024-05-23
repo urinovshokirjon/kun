9c0731096f86001476f7d78ee7c02624c2679c82
urinovshokirjon <urinovshokirjon93@gmail.com>
2024-05-23 20:04:08 +0500

    EmailHistoryEntity

---
+++ src/main/java/uz/urinov/kun/entity/EmailHistoryEntity.java
@@ -0,0 +1,14 @@
+package uz.urinov.kun.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+@Table("email_history")
+public class EmailHistoryEntity {
+
+}
---
+++ src/main/java/uz/urinov/kun/service/EmailHistoryService.java
@@ -0,0 +1,8 @@
+package uz.urinov.kun.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailHistoryService {
+
+}
